dc48ba7b84916fca7ec6183cd0c031a8d2e12eb9
dru18 <46237773+dru18@users.noreply.github.com>
2019-09-08 16:12:45 +0530

    Signed-off-by: dru18 <46237773+dru18@users.noreply.github.com>
    	modified:   backup.sh

--- backup.sh
+++ backup.sh
@@ -2,7 +2,7 @@
 
 if [ -z $1 ]; then
 
-echo "enter directory name to backup\n\n usage: backup ~/Videos/" | cowsay -f tux | toilet -f term --gay
+echo -e "enter directory name to backup\n\n usage: backup ~/Videos/" | cowsay -f tux | toilet -f term --gay
 
 exit
 
@@ -10,6 +10,6 @@ else
 
 of=backup$(date +%Y%m%d%s).tar.gz
 
-echo "Creating backup:\n\n$(tar -vczf $of $1)\n\nBackup done succesfully to $of." | cowsay -f tux | toilet -f term --gay
+echo -e "Creating backup:\n\n$(tar -vczf $of $1)\n\nBackup done succesfully to $of." | cowsay -f tux | toilet -f term --gay
 
 fi
