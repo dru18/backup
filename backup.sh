36e2e169123450a1238c12cbc78ea09955cdff59
dru18 <46237773+dru18@users.noreply.github.com>
2019-09-08 15:41:23 +0530

    Signed-off-by: dru18 <46237773+dru18@users.noreply.github.com>
    	modified:   backup.sh

--- backup.sh
+++ backup.sh
@@ -3,8 +3,8 @@
 if [ -z $1 ]; then
 echo -e "enter directory name to backup\n\n usage: backup ~/Videos/" | cowsay -f tux | toilet -f term --gay
 exit
-else
-of=myhome_directory_$(date +%Y%m%d%s).tar.gz
+else 
+of=backup$(date +%Y%m%d%s).tar.gz
 
 tar -czf $of $1
 
